0577739e22995863aa2e101596363d7b06132016
Naitian Zhou <nzhou2011@gmail.com>
2016-06-13 20:59:38 -0400

    Implement Arrow Key Navigation

--- scripts/tagbox.js
+++ scripts/tagbox.js
@@ -17,19 +17,18 @@ class TagInput {
 	}
 
 	handleKeyPress (e) {
+
 		if (e.which === 13 && this.input.value.length > 0 && !this.tags.includes(this.input.value)) {
-			console.log('New Tag: ' + this.input.value);
-			this.wrapper.appendChild(this.newTag(this.input.value));
-			this.tags.push(this.input.value);
-			this.input.value = '';
-			this.input.placeholder = '';
+			this.insertTag();
 		} else if (e.which === 8 && this.input.value.length === 0 && this.tags.length > 0) {
-			this.wrapper.removeChild(this.wrapper.lastChild);
-			console.log('deleted ' + this.tags.pop());
-			console.log(this.tags);
-			if (this.tags.length === 0)
-				this.input.placeholder = 'Let\'s Get Connected!';
+			this.backspace();
+		} else if (e.which === 37 && this.tags.length > 0) {
+			this.shiftLeft();
+		} else if (e.which === 39 && this.tags.length > 0) {
+			this.shiftRight();
 		}
+
+		this.input.style.width = ((this.input.value.length + 1) * 14) + 'px';
 	}
 
 	newTag (message) {
@@ -38,6 +37,38 @@ class TagInput {
 		tag.classList.add('tag');
 		tag.appendChild(messageText);
 		return tag;
+	}
+
+	insertTag () {
+
+		var tag = this.newTag(this.input.value);
+		this.wrapper.insertBefore(tag, this.input);
+
+		this.tags.push(this.input.value);
+		this.input.value = '';
+		this.input.placeholder = '';
+	}
+
+	backspace () {
+		this.wrapper.removeChild(this.input.previousSibling);
+		console.log('deleted ' + this.tags.pop());
+		console.log(this.tags);
+		if (this.tags.length === 0)
+			this.input.placeholder = 'Let\'s Get Connected!';
+	}
+
+	shiftLeft () {
+		console.log('shift left');
+		this.wrapper.insertBefore(this.input, this.input.previousSibling);
+		this.input.style.width = '1px';
+		this.input.focus();
+	}
+
+	shiftRight () {
+		console.log('shift right');
+		this.wrapper.insertBefore(this.input, this.input.nextSibling.nextSibling);
+		this.input.style.width = '1px';
+		this.input.focus();
 	}
 
 }
